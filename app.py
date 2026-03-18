b55be13ffe4e5f7b77dab954413429a3ebc5c866
wivictor21 <56314033+wivictor21@users.noreply.github.com>
2026-03-17 20:53:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -181,4 +181,4 @@ def export_pdf(topic):
                    headers={'Content-Disposition': f'attachment; filename="{topic}.pdf"'})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
